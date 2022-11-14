47e913bf3b25e6f4686535e10e40c10b8dc9286a
Nguyen Pham <phamduygiacnguyen9c@gmail.com>
2022-11-14 14:41:05 +0700

    Update shape2state.py

--- utils/shape2state.py
+++ utils/shape2state.py
@@ -2,6 +2,12 @@ import sys
 import torch
 
 model_path = sys.argv[1]
-dest_path = sys.argv[2]
+arch = sys.argv[2]
+dest_path = sys.argv[3]
+
 model = torch.load(model_path)
-torch.save(model.state_dict(), dest_path)
+state = {
+        'arch': arch,
+        'state_dict': model.state_dict()
+    }
+torch.save(state, dest_path)
